cd959c5556d537a8106d57e6c4916869e891b466
Gaganrattan Singh <138957257+CodeJediCheema@users.noreply.github.com>
2023-08-03 16:34:01 +0530

    App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/Header";
 import Body from "./src/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; //It is a library which is used to Route inside our react app.
 import About from "./src/About";
 import Contact from "./src/Contact";
 import Error from "./src/Error";
@@ -21,19 +21,20 @@ const AppLayout = () =>{
         </div>
     )
 }
-
+// It takes a list of paths {an object}, which contains the information .
 const appRouter = createBrowserRouter([
-    {
+    { // If the path is /, then load AppLayout
         path: "/",
         element: <AppLayout/>,
-        errorElement: <Error/>
+        errorElement: <Error/> // If the path is any random word, or which s not defined then load the Error
         
     },
-    {
+    { // If the path is /about , then load About
         path:"/about",
         element: <About/>
     },
     {
+        // If the path is /contact, then load Contact
         path:"/contact",
         element:<Contact/>
     }
@@ -41,5 +42,5 @@ const appRouter = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter}/>); // RouterProvider provides the routing configuration to our app.
 
