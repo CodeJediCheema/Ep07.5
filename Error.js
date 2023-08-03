e886db89c507d0ac4d1735d1c98550c1a9653757
Gaganrattan Singh <138957257+CodeJediCheema@users.noreply.github.com>
2023-08-03 16:36:44 +0530

    Error.js

--- Error.js
+++ Error.js
@@ -1,15 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom"; // React-router-dom gives us some cool functionality. An important HOOK.By this we can get more details about the error(nature of error)
 
 const Error = () =>{
     const err = useRouteError();
-    console.log(err);
+    console.log(err);// it will give us all the details of the error like status, statustext, etc etc
     return(
         <div className="error-container">
             <div className="error">
                 <h1>OOPS!!!</h1>
                 
                 <h2>Error {err.status} {err.statusText}</h2>
-                <h2>{err.error.message}</h2>
+                <h2>{err.error.message}</h2> // here we can display them according to our needs
             </div>
         </div>
     )
